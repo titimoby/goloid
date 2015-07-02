131d9f016fb56d61898531c83994ac722e88d589
Thierry Chantier <titimoby@gmail.com>
2015-07-02 08:56:04 +0200

    modify launcher. fix missing return on delegate method

--- go.sh
+++ go.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-#
-
-RC=1
-#trap "echo CTRL-C was pressed" 2
-trap "exit 1" 2
-while [ $RC -ne 0 ] ; do
-#   golo golo --classpath jars/*.jar --files lib/*.golo main.golo
-   golo golo --classpath jars/*.jar --files main.golo
-   RC=$?
-done
+golo golo --classpath jars/*.jar --files $1.golo
